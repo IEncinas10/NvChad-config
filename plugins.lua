66a04bdf2a27181f08f429d1e059772df806f326
Ignacio Encinas <ignacio@iencinas.com>
2024-07-14 13:01:25 +0200

    typst-preview: lazy-load on opening typst files

--- plugins.lua
+++ plugins.lua
@@ -144,7 +144,7 @@ local plugins = {
   },
   {
     'chomosuke/typst-preview.nvim',
-    lazy = false, -- or ft = 'typst'
+    ft = 'typst',
     version = '0.3.*',
     build = function() require 'typst-preview'.update() end,
   },
