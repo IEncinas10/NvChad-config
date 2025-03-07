0a8964ab91189440ea7d3020ba2e3d53754f8972
Ignacio Encinas <ignacio@iencinas.com>
2025-03-07 18:59:29 +0100

    lspconfig: clangd: set background-index-priority to background

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -62,7 +62,7 @@ lspconfig.clangd.setup {
   cmd = {
     "clangd",
     "--background-index",
-    "--background-index-priority=low",
+    "--background-index-priority=background",
     -- by default, clang-tidy use -checks=clang-diagnostic-*,clang-analyzer-*
     -- to add more checks, create .clang-tidy file in the root directory
     -- and add Checks key, see https://clang.llvm.org/extra/clang-tidy/
